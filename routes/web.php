42f01d829e3f004b1fa58c6c7cfd040889e023f0
Agustin <agusting.moldavsky@gmail.com>
2026-01-22 07:40:11 -0300

    Feature/SendEmail

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EnviarCorreo;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('enviar-correo', function () {
+    // Lógica para enviar el correo electrónico
+    Mail::to('agusting.moldavsky@gmail.com')->send(new EnviarCorreo());
+    
+    return 'Correo enviado';
+})->name('enviar-correo');
